46c8ab920efe4d547bf564b8b3e05f88289fc212
alexchirol <alexandrechirol62@gmail.com>
2022-12-05 15:32:56 +0100

    Ajout de fichier
    Ajout du fichier operators pour la suite du cours Python.

---
+++ operators.py
@@ -0,0 +1,46 @@
+import random
+import math
+
+# = affectation
+foo = 123
+
+# + addition
+foo = 123 + 42
+foo = foo + 42 # foo +=42 opérateur d'indcrémentation
+
+# - soustraction 
+foo = 123 - 42
+foo = foo - 42 # foo -=42 opérateur de décrémentation
+
+# / division 
+foo = 123 / 42
+foo = foo / 42 # foo /=42
+print(foo)
+print(type(foo))
+
+# // division entière 
+foo = 123 // 42
+foo = foo // 42 # foo //=42
+print(foo)
+print(type(foo))
+
+# % modulo
+foo = 4 % 3
+print(foo)
+foo = random.randint(1, 100)
+print(foo)
+print(foo % 2)
+
+# * multiplication
+
+
+# ** puissance
+foo = 2 ** 2
+foo = 2 ** 3
+foo = 2 ** 4
+foo = 2 ** 5
+foo = 2 ** 6
+print(foo)
+foo = math.sqrt(4)
+
+# sqrt() racine carré
